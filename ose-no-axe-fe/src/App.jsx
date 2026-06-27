819770cbe4108bc7349c77345de1750b71f95051
brasselotti <brasselottif@gmail.com>
2026-06-27 08:49:49 -0300

    feat: instalar e configurar React Router

--- ose-no-axe-fe/src/App.jsx
+++ ose-no-axe-fe/src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom'
+import Cadastro from './pages/Cadastro'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import Cadastros from './apps/cadastros/pages'
+import Novo from './apps/cadastros/pages/Novo'
+import Editar from './apps/cadastros/pages/Editar'
+import Mensalidades from './apps/mensalidades/pages'
+import Registrar from './apps/mensalidades/pages/Registrar'
+import Analisar from './apps/mensalidades/pages/Analisar'
+import List from './apps/mensalidades/pages/list'
+import Detail from './apps/mensalidades/pages/list/Detail'
+
+function ProtectedRoute() {
+  return <Outlet />
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <h1>OSÉ NO AXÉ</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<ProtectedRoute />}>
+          <Route path="home" element={<Home />} />
+          <Route path="cadastros" element={<Cadastros />} />
+          <Route path="cadastros/novo" element={<Novo />} />
+          <Route path="cadastros/editar" element={<Editar />} />
+          <Route path="mensalidades" element={<Mensalidades />} />
+          <Route path="mensalidades/registrar" element={<Registrar />} />
+          <Route path="mensalidades/analisar" element={<Analisar />} />
+          <Route path="mensalidades/list" element={<List />} />
+          <Route path="mensalidades/list/:id" element={<Detail />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
